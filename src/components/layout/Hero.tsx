3d7707ddf1b3738f03e794af01cc6d23ae3790a7
Zayan <khanzayan_123@hotmail.com>
2026-04-26 13:59:24 -0400

    Landing page Changes

---
+++ src/components/layout/Hero.tsx
@@ -0,0 +1,21 @@
+export default. function Hero() {
+    return(
+        <section className = "flex flex-col items-center justify-center text-center py-32">
+            <h1 className = "text-5xl font-bold mb-6">
+                The Master Guidance of DSA (Data Structure's and Algoritm) from First Principles
+            </h1>
+            <p clasName = "text-gray-400 max-w-xl mb-8">
+
+            </p>
+            <div className="flex gap-4">
+                <button className="bg-primary px-6 py-3 rounded-lg">
+                Start Learning
+                </button>
+
+                <button className="border border-gray-600 px-6 py-3 rounded-lg">
+                Explore Visualizer
+                </button>
+            </div>
+        </section>
+    );
+}
---
+++ src/tailwind.config.ts
@@ -0,0 +1,9 @@
+theme: {
+  extend: {
+    colors: {
+      primary: "#7C3AED", // purple
+      secondary: "#06B6D4", // cyan
+      background: "#0B0F19",
+    },
+  },
+}
